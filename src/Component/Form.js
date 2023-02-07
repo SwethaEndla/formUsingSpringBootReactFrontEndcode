d79ca2b6ebbcbe63dcb0704a8207da05a18f416f
SwethaEndla <102942301+SwethaEndla@users.noreply.github.com>
2023-02-07 15:24:17 +0530

    Update Form.js

--- src/Component/Form.js
+++ src/Component/Form.js
@@ -41,7 +41,14 @@ const Form = () => {
 
       },
       body: JSON.stringify(data)
-    })
+    }) .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+
   };
 
   return ( 
